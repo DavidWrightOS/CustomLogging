536c0c52914dda7d35aadd3296f3b8dd4f5cdf1e
David Wright <davidtwright89@gmail.com>
2025-05-31 00:50:48 -0400

    [Feature] Expose LogManager methods

--- Sources/CustomLogging/LogManager.swift
+++ Sources/CustomLogging/LogManager.swift
@@ -17,44 +17,44 @@ public class LogManager {
         self.services = services
     }
 
-    func trackEvent(eventName: String, parameters: [String: Any]? = nil, type: LogType = .analytic) {
+    public func trackEvent(eventName: String, parameters: [String: Any]? = nil, type: LogType = .analytic) {
         let event = AnyLoggableEvent(eventName: eventName, parameters: parameters, type: type)
         for service in services {
             service.trackEvent(event: event)
         }
     }
 
-    func trackEvent(event: AnyLoggableEvent) {
+    public func trackEvent(event: AnyLoggableEvent) {
         for service in services {
             service.trackEvent(event: event)
         }
     }
 
-    func trackEvent(event: LoggableEvent) {
+    public func trackEvent(event: LoggableEvent) {
         for service in services {
             service.trackEvent(event: event)
         }
     }
 
-    func trackScreenView(event: LoggableEvent) {
+    public func trackScreenView(event: LoggableEvent) {
         for service in services {
             service.trackScreenView(event: event)
         }
     }
 
-    func identifyUser(userId: String, name: String?, email: String?) {
+    public func identifyUser(userId: String, name: String?, email: String?) {
         for service in services {
             service.identifyUser(userId: userId, name: name, email: email)
         }
     }
 
-    func addUserProperties(dict: [String: Any], isHighPriority: Bool = false) {
+    public func addUserProperties(dict: [String: Any], isHighPriority: Bool = false) {
         for service in services {
             service.addUserProperties(dict: dict, isHightPriority: isHighPriority)
         }
     }
 
-    func deleteUserProfile() {
+    public func deleteUserProfile() {
         for service in services {
             service.deleteUserProfile()
         }
